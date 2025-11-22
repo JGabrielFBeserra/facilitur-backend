3b416d450ca60445a13e56545222cf0298c0ebbf
Robert-Cortez-Rudi <robertcortezrudi@gmail.com>
2025-11-22 15:57:20 -0300

    FEAT: adição da lista de usuários ativos

--- api/src/main/java/com/facilitur/api/repository/UsuarioNewsletterRepository.java
+++ api/src/main/java/com/facilitur/api/repository/UsuarioNewsletterRepository.java
@@ -4,9 +4,11 @@ import com.facilitur.api.models.entities.UsuarioNewsletter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsuarioNewsletterRepository extends JpaRepository<UsuarioNewsletter, Long> {
     Optional<UsuarioNewsletter> findByEmail(String email);
+    List<UsuarioNewsletter> findByAtivoTrue();
 }
