f92cb02281bda517fc48a04aa2ed1f270377cb55
Robert-Cortez-Rudi <robertcortezrudi@gmail.com>
2025-11-22 15:54:09 -0300

    FEAT: config para envio periódico de emails

---
+++ api/src/main/java/com/facilitur/api/configs/SchedulerConfig.java
@@ -0,0 +1,11 @@
+package com.facilitur.api.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+
+}
