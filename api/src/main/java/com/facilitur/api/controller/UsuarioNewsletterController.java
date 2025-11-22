9adc51f6949ae9972ac77134ba0dcc8c3d3ed454
Robert-Cortez-Rudi <robertcortezrudi@gmail.com>
2025-11-22 15:55:20 -0300

    FEAT: envio de email quando o usuário se registra na newsletter

--- api/src/main/java/com/facilitur/api/controller/UsuarioNewsletterController.java
+++ api/src/main/java/com/facilitur/api/controller/UsuarioNewsletterController.java
@@ -2,6 +2,7 @@ package com.facilitur.api.controller;
 
 import com.facilitur.api.DTO.usuario_newsletter.*;
 import com.facilitur.api.models.entities.UsuarioNewsletter;
+import com.facilitur.api.service.EmailService;
 import com.facilitur.api.service.UsuarioNewsletterService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,8 @@ public class UsuarioNewsletterController {
     @Autowired
     UsuarioNewsletterService service;
 
+    @Autowired
+    private EmailService emailService;
 
     @PostMapping
     public ResponseEntity<?> cadastrarOuAtivarUsuarioNewsletter(@RequestBody @Valid CreateRequestDTO dados, UriComponentsBuilder uriBuilder) {
@@ -25,8 +28,27 @@ public class UsuarioNewsletterController {
         CreateResponseDTO respostaDaRequisicao = service.cadatrarUsuarioNaNewsletter(dados);
         UsuarioNewsletter usuario = respostaDaRequisicao.usuario();
 
+
+        String assunto;
+        String corpo;
+
         if (respostaDaRequisicao.PostForCreateNewUsuarioResponseDTO()) {
-            var body = Map.<String, Object>of (
+            assunto = "Bem-vindo de volta à Newsletter Facilitur!";
+            corpo = "Olá " + usuario.getNome() + "!" +
+                    "\n\nSeu cadastro na newsletter foi reativado com sucesso!\n\n" +
+                    "Agora você voltará a receber nossos avisos e eventos semanais.\n\n" +
+                    "Seja bem-vindo novamente!\nEquipe Facilitur";
+        } else {
+            assunto = "Bem-vindo à Newsletter Facilitur!";
+            corpo = "Olá " + usuario.getNome() + "!" + "!\n\nSeu cadastro na nossa newsletter foi realizado com sucesso!\n\n" +
+                    "A partir de agora você receberá semanalmente os eventos da Facilitur.\n\n" +
+                    "Obrigado por se juntar a nós!\nEquipe Facilitur";
+        }
+
+        emailService.enviarEmail(usuario.getEmail(), assunto, corpo);
+
+        if (respostaDaRequisicao.PostForCreateNewUsuarioResponseDTO()) {
+            var body = Map.<String, Object>of(
                     "usuario", new PatchReativarUsuarioDTO(usuario.getEmail(), usuario.isAtivo()),
                     "mensagem", respostaDaRequisicao.mensagem()
             );
@@ -39,8 +61,8 @@ public class UsuarioNewsletterController {
             );
             return ResponseEntity.created(uri).body(body);
         }
-
     }
+
 
     @PatchMapping
     public ResponseEntity<PatchDesativarUsuarioResponseDTO> desativarUsuarioNewsLetter(@RequestBody @Valid PatchDesativarUsuarioRequestDTO email){
@@ -50,8 +72,6 @@ public class UsuarioNewsletterController {
         }
         return ResponseEntity.ok(resposta);
     }
-
-
 
 }
 
